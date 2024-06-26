80d182c61474f51388c5090ad5e078de7b000dc6
少泽 <xshaoze@qq.com>
2024-06-27 00:21:13 +0800

    Delete frontend/wails js directory

--- app.go
+++ app.go
@@ -17,7 +17,6 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
